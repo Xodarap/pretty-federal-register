68d5e88ab80095777af22a469179ebfe1bda7cd9
Ben West <bwest@healthefilings.com>
2017-11-22 16:21:16 -0500

    working commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Pretty Federal Register
+=================
+
+This chrome extension adds several navigation features to the FR site:
+
+1. It adds hyperlinks to the table of contents, making the items in the TOC link to the relevant parts of the document
+2. It displays the fully qualified path of headers (e.g. "II.B.1" instead of just "1.")
+
+It is based around the implicit format that Medicare uses and, as this is merely
+an implicit format, may or may not work for other agencies.
+
+**License**
+Copyright (C) 2017
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ content.js
@@ -0,0 +1,63 @@
+var bulletExpression = /^((V?I{1,3}\.)|([A-Z]\.)|(\d+\.)|([a-z]\.)|(\(\d\))|(\([a-z]\)))/;
+var linkMap = {};
+
+function modifyHeaders() {
+  var headers = getHeaders();
+  var stack = new Stack();
+  headers.each(function (header) {
+    stack.modifyStack(header.bullet)
+    var bulletString = stack.toString();
+    linkMap[bulletString] = header.element.id;
+    var locationLink = ' [<a href="#' + header.element.id + '">' + bulletString + '</a>]'
+    header.element.innerHTML = header.element.innerHTML + locationLink;
+  }, this)
+}
+
+function getHeaders() {
+  var headers = _(document.querySelectorAll('h1,h2,h3,h4'));
+  return parseIdentifiers(headers);
+}
+
+
+function modifyTableOfContents() {
+  var contents = getContents();
+  var stack = new Stack();
+  parseIdentifiers(_(contents)).each(function (item) {
+    stack.modifyStack(item.bullet);
+    var bulletString = stack.toString();
+    var id = linkMap[bulletString];
+
+    item.element.innerHTML = item.element.innerHTML +
+        ' [<a href="#' + id + '">' + bulletString + '</a>]';
+  })
+}
+
+function getContents() {
+  var start = _(document.getElementsByTagName('h2')).find(function (element) {
+    return element.innerText == "Table of Contents";
+  })
+  if (start == undefined) {
+    return [];
+  }
+  var paragraphs = [];
+  var next = start.nextElementSibling;
+  while (next.nodeName == 'P') {
+    paragraphs.push(next);
+    next = next.nextElementSibling;
+  }
+  return paragraphs;
+}
+
+function parseIdentifiers(headers) {
+  return headers.map(function (header) {
+    var text = header.innerHTML;
+    var bullet = bulletExpression.exec(text);
+    if (bullet == undefined) {
+      return;
+    }
+    return {bullet: bullet[0], element: header};
+  }).compact();
+}
+
+modifyHeaders();
+modifyTableOfContents();
