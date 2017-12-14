d568c6454b43666323664f5e1bae19aa4c0b1f20
Ben West <bwest@healthefilings.com>
2017-12-14 13:31:23 -0600

    reformat tests

--- test/stack.js
+++ test/stack.js
@@ -1,97 +1,65 @@
 var assert = require('assert');
-var _ =require('../lodash.core.js');
-var s =require('../stack');
-describe('Stack', function() {
-  describe('#BulletLevel', function() {
-    it('should parse levels', function() {
-
-      var stack = new s();
-      stack.modifyStack('I.');
-      stack.modifyStack('A.');
-      assert.deepEqual(['*I.', '**A.'], stack.toTree());
-      assert.equal('I.A.', stack.toString());
-    });
-    it('should Handle skipping levels', function() {
-      var stack = new s();
-      stack.modifyStack('I.');
-      stack.modifyStack('1.');
-      assert.deepEqual(['*I.', '**(Placeholder)', '***1.'], stack.toTree());
-    });
-
-    it('should handle ambiguous Roman numerals1', function() {
-      var stack = new s();
-      var expected = ['I.', 'A.', '1.', 'a.', '(1)','(a)','(i)']
-      _(expected).each(function(bullet) {
-        stack.modifyStack(bullet);
-      })
-      assert.deepEqual(['*I.', '**A.', '***1.', '****a.', '*****(1)','******(a)','*******(i)'],
-          stack.toTree());
-    });
-    it('should handle ambiguous Roman numerals2', function() {
-      var stack = new s();
-      var expected = ['I.', 'A.', '1.', 'a.', '(1)','(h)','(i)']
-      _(expected).each(function(bullet) {
-        stack.modifyStack(bullet);
-      })
-      assert.deepEqual(['*I.', '**A.', '***1.', '****a.', '*****(1)',
-        '******(h)','******(i)'], stack.toTree());
-    });
-    it('should handle ambiguous Roman numerals2-1', function() {
-      var stack = new s();
-      var expected = ['I.', 'A.', '1.', 'a.', '(1)','(f)', '(i)','(h)','(v)']
-      _(expected).each(function(bullet) {
-        stack.modifyStack(bullet);
-      })
-      assert.deepEqual(['*I.', '**A.', '***1.', '****a.', '*****(1)',
-        '******(f)','*******(i)','******(h)','*******(v)'], stack.toTree());
-    });
-    it('should handle ambiguous Roman numerals3', function() {
-      var stack = new s();
-      var expected = ['I.', 'A.', 'II.']
-      _(expected).each(function(bullet) {
-        stack.modifyStack(bullet);
-      })
-      assert.deepEqual(['*I.', '**A.', '*II.'], stack.toTree());
-      assert.equal('II.', stack.toString());
-    });
-    it('should handle ambiguous Roman numerals4', function() {
-      var stack = new s();
-      var expected = ['I.', 'A.', '1.', 'a.', '(1)','(a)','(b)','(i)', 'B.']
-      _(expected).each(function(bullet) {
-        stack.modifyStack(bullet);
-      })
-      assert.deepEqual(['*I.', '**A.', '***1.', '****a.', '*****(1)',
-        '******(a)','******(b)','*******(i)', '**B.'],
-          stack.toTree());
-    });
-    it('should handle ambiguous Roman numerals5', function() {
-      var stack = new s();
-      var expected = ['I.', 'A.', '1.', 'a.', '(1)','(a)','(b)','(i)', '(c)','B.']
-      _(expected).each(function(bullet) {
-        stack.modifyStack(bullet);
-      })
-      assert.deepEqual(['*I.', '**A.', '***1.', '****a.', '*****(1)',
-        '******(a)','******(b)','*******(i)','******(c)', '**B.'],
-          stack.toTree());
-    });
-    it('should handle ambiguous Roman numerals6', function() {
-      var stack = new s();
-      var expected = ['I.', 'A.', '1.', 'a.', '(1)','(a)','(b)','(i)', '(c)','B.']
-      _(expected).each(function(bullet) {
-        stack.modifyStack(bullet);
-      })
-      assert.deepEqual(['*I.', '**A.', '***1.', '****a.', '*****(1)',
-        '******(a)','******(b)','*******(i)','******(c)', '**B.'],
-          stack.toTree());
-    });
-    it('should handle ambiguous Roman numerals7', function() {
-      var stack = new s();
-      var expected = ['I.', 'H.', 'I.']
-      _(expected).each(function(bullet) {
-        stack.modifyStack(bullet);
-      })
-      assert.deepEqual(['*I.', '**H.', '**I.'],
-          stack.toTree());
+var _ = require('../lodash.core.js');
+var s = require('../stack');
+describe('Stack', function () {
+  describe('#BulletLevel', function () {
+    tests = [
+      {
+        input: ['I.', 'A.', '1.', 'a.', '(1)', '(a)', '(i)'],
+        output: ['*I.', '**A.', '***1.', '****a.', '*****(1)', '******(a)', '*******(i)']
+      },
+      {
+        input: ['I.', 'A.', '1.', 'a.', '(1)', '(f)', '(i)', '(h)', '(v)'],
+        output: ['*I.', '**A.', '***1.', '****a.', '*****(1)',
+          '******(f)', '*******(i)', '******(h)', '*******(v)']
+      },
+      {
+        input: ['I.', 'A.', 'II.'],
+        output: ['*I.', '**A.', '*II.']
+      },
+      {
+        input: ['I.', '1.'],
+        output: ['*I.', '**(Placeholder)', '***1.']
+      },
+      {
+        input: ['I.', 'A.', '1.', 'a.', '(1)', '(a)', '(b)', '(i)', 'B.'],
+        output: ['*I.', '**A.', '***1.', '****a.', '*****(1)',
+          '******(a)', '******(b)', '*******(i)', '**B.']
+      },
+      {
+        input: ['I.', 'A.', '1.', 'a.', '(1)', '(a)', '(b)', '(i)', '(c)', 'B.'],
+        output: ['*I.', '**A.', '***1.', '****a.', '*****(1)',
+          '******(a)', '******(b)', '*******(i)', '******(c)', '**B.']
+      },
+      {
+        input: ['I.', 'A.', '1.', 'a.', '(1)', '(a)', '(b)', '(i)', 'B.'],
+        output: ['*I.', '**A.', '***1.', '****a.', '*****(1)',
+          '******(a)', '******(b)', '*******(i)', '**B.']
+      },
+      {
+        input: ['I.', 'A.', '1.', 'a.', '(1)', '(a)', '(b)', '(i)', '(c)', 'B.'],
+        output: ['*I.', '**A.', '***1.', '****a.', '*****(1)',
+          '******(a)', '******(b)', '*******(i)', '******(c)', '**B.']
+      },
+      {
+        input: ['I.', 'A.', '1.', 'a.', '(1)', '(a)', '(b)', '(i)', 'B.'],
+        output: ['*I.', '**A.', '***1.', '****a.', '*****(1)',
+          '******(a)', '******(b)', '*******(i)', '**B.']
+      },
+      {
+        input: ['I.', 'H.', 'I.'],
+        output: ['*I.', '**H.', '**I.']
+      }
+    ];
+    _(tests).each(function (test) {
+      it('Test: ' + test.input.join(), function () {
+        var stack = new s();
+        _(test.input).each(function (bullet) {
+          stack.modifyStack(bullet);
+        })
+        assert.deepEqual(test.output,
+            stack.toTree());
+      });
     });
   });
 });
